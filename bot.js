c50cff44e096c26f3fbe05f6c50bc6066ee1a848
HailTheCzar <71359784+HailTheCzar@users.noreply.github.com>
2021-08-27 00:22:36 -0400

    Updated bot.js with new functions to collect messages

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, User } = require("discord.js");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
@@ -10,7 +10,25 @@ client.once("ready", () => {
 
 client.on("messageCreate", (msg) => {
   if (msg.content === "!bet") {
-    msg.reply("Place your bet you degenerate...");
+    msg.reply("Place your bet you degenerate...").then(() => {
+      const filter = (m) => msg.author.id === m.author.id;
+      if (msg.author.id != client.user.id) {
+        const collector = msg.channel.createMessageCollector({
+          filter,
+          time: 15000,
+        });
+        let bets = [];
+        collector.on("collect", (m) => {
+          console.log(`Collected ${m.content}`);
+          msg.channel.send(`Collected ${m.content}`);
+        });
+
+        collector.on("end", (collected) => {
+          console.log(`Collected ${collected.size} items`);
+          msg.channel.send(`Collected ${collected.size} items`);
+        });
+      }
+    });
   }
 });
 
